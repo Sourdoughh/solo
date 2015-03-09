d7999b6da5b23badef67d79dabca6738f6ea4489
JasonLy <jason.ly@gmail.com>
2015-03-09 14:39:58 -0700

    Implement image only append

--- src/app.js
+++ src/app.js
@@ -1,19 +1,28 @@
 $(document).ready(function(){
 
   var accessImages = function(data){
-    for(var i = 0; i < 14; i++){
-      $('.images').append("<li><img src=" + data.data[i].images.standard_resolution.url + "></li>");
-    }
-    console.log('hey')
-  }
+    var total = 14;
+    for(var i = 0; i < total; i++){
 
-  // display images only up to 14 spaces
+      if(data.data[i].type === 'video'){
+        total++;
+      };
+
+      if(data.data[i].type !== 'video'){
+        var img = '<li><img src=' + data.data[i].images.standard_resolution.url + '></li>'
+        $('.images').append(img);
+        console.log(data)
+      };
+    }
+
+  };
 
   // laythem out in an order in the correct orientation
 
   // allow the user to change the image location
 
   // get images from instagram user
+
   $.ajax({
     type: "GET",
     dataType: "jsonp",
