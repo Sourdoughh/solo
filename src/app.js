1c60d7d3fa5bfb56477321e06dd995f9faac17a8
JasonLy <jason.ly@gmail.com>
2015-03-09 14:56:55 -0700

    Implement d3 lib

--- src/app.js
+++ src/app.js
@@ -11,12 +11,9 @@ $(document).ready(function(){
       if(data.data[i].type !== 'video'){
         var img = '<li><img src=' + data.data[i].images.standard_resolution.url + '></li>'
         $('.images').append(img);
-        console.log(data)
       };
     }
-
   };
-
   // laythem out in an order in the correct orientation
 
   // allow the user to change the image location
